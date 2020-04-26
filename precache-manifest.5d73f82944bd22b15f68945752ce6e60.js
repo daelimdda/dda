323c33dea660f6dc2b841d72529cdd1991e12a20
LEE <kosnsn@gmail.com>
2020-04-26 19:32:27 +0900

    Updates

---
+++ precache-manifest.5d73f82944bd22b15f68945752ce6e60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f457d8aa0b25c3fedf13dc75e3ed768",
+    "url": "/dda/index.html"
+  },
+  {
+    "revision": "d3a6691665ec4f8493b2",
+    "url": "/dda/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "2e03437e8670eb2f666a",
+    "url": "/dda/static/css/main.7a2736dc.chunk.css"
+  },
+  {
+    "revision": "d3a6691665ec4f8493b2",
+    "url": "/dda/static/js/2.0e0f9683.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/dda/static/js/2.0e0f9683.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e03437e8670eb2f666a",
+    "url": "/dda/static/js/main.5d777d38.chunk.js"
+  },
+  {
+    "revision": "08f059d95c69fb03d364",
+    "url": "/dda/static/js/runtime-main.544ccc37.js"
+  }
+]);
