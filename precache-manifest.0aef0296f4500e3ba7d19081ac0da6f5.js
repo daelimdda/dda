2ca60ed883fc817d9c4b297e24a7b04e339b33a6
LEE <kosnsn@gmail.com>
2020-05-11 19:27:31 +0900

    Updates

--- precache-manifest.0aef0296f4500e3ba7d19081ac0da6f5.js
+++ precache-manifest.0aef0296f4500e3ba7d19081ac0da6f5.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c9bc9fe4eee950cfd8d367e663bfb3d6",
+    "revision": "92d6ce51d1c030b9e2ca87b9f965020f",
     "url": "/dda/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/dda/static/css/2.69121389.chunk.css"
   },
   {
-    "revision": "4e337f09f2f10a98ffc6",
+    "revision": "bc575253b9d4c40fa3f0",
     "url": "/dda/static/css/main.99c381ca.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/dda/static/js/2.247db651.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4e337f09f2f10a98ffc6",
-    "url": "/dda/static/js/main.50005574.chunk.js"
+    "revision": "bc575253b9d4c40fa3f0",
+    "url": "/dda/static/js/main.f90f201e.chunk.js"
   },
   {
     "revision": "08f059d95c69fb03d364",
