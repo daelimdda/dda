bdfef68e37d1af140361fa8f4524d5a6f796e516
JS <e92ku@naver.com>
2020-06-13 13:28:03 +0900

    quiz update

--- src/component/board.js
+++ src/component/board.js
@@ -1,11 +1,57 @@
 import React from 'react';
 import BigText from './BigText';
+import { Link } from '@version/react-router-v3';
+import "./board.css";
 
 const Board = () => {
     return (
-        <div>
-           <BigText>게시판 페이지</BigText>
+        <div className="boarddiv">
+           <BigText>게시판</BigText>
+           <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <td width="10%">게시글번호</td>
+                                    <td width="30%">제목</td>
+                                    <td width="10%">글쓴이</td>
+                                    <td width="10%">등록날짜</td>
+                                    <td width="10%">조회수</td>
+                                    <td width="10%">추천수</td>
+                                </tr>
+                                <tr>
+                                    <td width="10%">1</td>
+                                    <td width="30%">제목</td>
+                                    <td width="10%">글쓴이</td>
+                                    <td width="10%">등록날짜</td>
+                                    <td width="10%">조회수</td>
+                                    <td width="10%">추천수</td>
+                                </tr>
+                                <tr>
+                                    <td width="10%">2</td>
+                                    <td width="30%">제목</td>
+                                    <td width="10%">글쓴이</td>
+                                    <td width="10%">등록날짜</td>
+                                    <td width="10%">조회수</td>
+                                    <td width="10%">추천수</td>
+                                </tr>
+                                <tr>
+                                    <td width="10%">3</td>
+                                    <td width="30%">제목</td>
+                                    <td width="10%">글쓴이</td>
+                                    <td width="10%">등록날짜</td>
+                                    <td width="10%">조회수</td>
+                                    <td width="10%">추천수</td>
+                                </tr>
+                            </thead>
+                            
+                        </table>
+                        
+                        <Link to="/dda/BoardWriteForm">
+                        
+                        <input type="button" value="글쓰기" id="btn_newcontent" class="btn"></input>
+                        
+                        </Link>
         </div>
+        
     );
 };
 
--- src/component/quiz.js
+++ src/component/quiz.js
@@ -5,7 +5,9 @@ class quiz extends Component {
     word : '이주상',
     value : '',
     time:0,
-    result : '',
+    result : [''],
+    hae:[['도로교통법 제73조(교통안전교육)③ 다음 각 호의 어느 하나에 해당하는 사람이 지방경찰청장에게 신청하는 경우에는 대통령령으로 정하는 바에 따라 특별교통안전 권장교육을 받을 수 있다. 이 경우 권장교육을 받기 전 1년 이내에 해당 교육을 받지 아니한 사람에 한정한다.' ]
+        ,['18세 미만(원동기장치자전거의 경우에는 16세미만)인 사람은 운전면허를 받을 수 없고, 제1종 대형면허 및 제1종 특수면허를 받으려는 경우로서 19세 미만이거나 자동차(이륜자동차를 제외한다)의 운전경험이 1년 미만인 사람은 운전면허를 받을 수 없다.']],
     answer: ['1','4','4'],
     bogi:[["2년","3년","4년","5년"],
           ["운전면허를 받은 사람 중 교육을 받으려는 날에 65세 이상인 사람","운전면허효력 정지처분을 받고 그 정지기간이 끝나지 아니한 초보운전자로서 특별교통안전      의무교육을 받은 사람","교통법규 위반 등으로 인하여 운전면허효력 정지처분을 받을 가능성이 있는 사람","적성검사를 받지 않아 운전면허가 취소된 사람"],
@@ -15,6 +17,8 @@ class quiz extends Component {
           "다음 중 특별교통안전 권장교육 대상자로 아닌 사람은?",
           "도로교통법상 제2종 소형면허를 취득할 수 있는 연령 기준으로 맞는 것은? ",""
          ]
+         ,
+         a:4
   };
 
   onSubmitForm= (e) =>{
@@ -23,15 +27,16 @@ class quiz extends Component {
     if(this.state.answer[this.state.time] === this.state.value){
         
       this.setState({
-        result:'딩동댕',
+      
         value:'',
         time: this.state.time +1
       });
       
     }else {
       this.setState({
-        result:'땡',
+        result:this.state.result.concat(this.state.time),
         value:'',
+        time: this.state.time +1
       });
     
     }
@@ -40,23 +45,56 @@ class quiz extends Component {
     this.setState({value:e.target.value});
   }
   input;
-
+  
  
   render() {
+
     return (
       <div className="d1">
       <div className="title">운전면허 자격증 기출문제</div>
-      <div className="quizdiv">
-      <h1>{this.state.time+1}. </h1><h4>{this.state.quiz[this.state.time]}</h4><br/><br/>
+      
+       
+        { this.state.time < 3 ?
+         <div className="quizdiv">
+        <h1>{this.state.time+1}. </h1><h4>{this.state.quiz[this.state.time]}</h4><br/><br/>
         <br/>
         <form onSubmit={this.onSubmitForm}>
           <button className="btnQ" value = "1" onClick={this.onChange}>1</button><h5>{this.state.bogi[this.state.time][0]}</h5><br/><br/>
           <button className="btnQ" value = "2" onClick={this.onChange}>2</button><h5>{this.state.bogi[this.state.time][1]}</h5><br/><br/>
           <button className="btnQ" value = "3" onClick={this.onChange}>3</button><h5>{this.state.bogi[this.state.time][2]}</h5><br/><br/>
           <button className="btnQ" value = "4" onClick={this.onChange}>4</button><h5>{this.state.bogi[this.state.time][3]}</h5><br/><br/>
-          <br/>{this.state.result}<br/>
         </form>
-      </div>
+        </div>
+        :  <div className="quizdiv">
+          <h2>틀린 문제</h2><br/>
+          { this.state.result[1]!==null  ?
+          <div>
+          <h1>{this.state.result[1]+1}. </h1><h4>{this.state.quiz[this.state.result[1]]}</h4><br/><br/>
+          <form onSubmit={this.onSubmitForm}>
+          <button className="btnQ" value = "1" >1</button><h5>{this.state.bogi[this.state.result[1]][0]}</h5><br/><br/>
+          <button className="btnQ" value = "2" >2</button><h5>{this.state.bogi[this.state.result[1]][1]}</h5><br/><br/>
+          <button className="btnQ" value = "3" >3</button><h5>{this.state.bogi[this.state.result[1]][2]}</h5><br/><br/>
+          <button style={{background:"red"}}className="btnQ" value = "4" >4</button><h5>{this.state.bogi[this.state.result[1]][3]}</h5><br/><br/>
+          </form>
+          <h2>해설</h2> {this.state.hae[0]}
+          </div>
+          : "" }
+        { this.state.result[2]!==null  ?
+          <div>
+          <h1>{this.state.result[2]+1}. </h1><h4>{this.state.quiz[this.state.result[2]]}</h4><br/><br/>
+          <form onSubmit={this.onSubmitForm}>
+          <button className="btnQ" value = "1" >1</button><h5>{this.state.bogi[this.state.result[2]][0]}</h5><br/><br/>
+          <button className="btnQ" value = "2" >2</button><h5>{this.state.bogi[this.state.result[2]][1]}</h5><br/><br/>
+          <button className="btnQ" value = "3" >3</button><h5>{this.state.bogi[this.state.result[2]][2]}</h5><br/><br/>
+          <button style={{background:"red"}} className="btnQ" value = "4" >4</button><h5>{this.state.bogi[this.state.result[2]][3]}</h5><br/><br/>
+          </form>
+          <h2>해설</h2> {this.state.hae[1]}
+          </div>
+          : "" }
+          
+        </div>
+        }
+        
   
       </div>
     );
